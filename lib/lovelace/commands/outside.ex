3bc77131a901c6f47d451d263b1cf5d161ab1b63
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-01-30 18:59:41 -0300

    commander and external commands

---
+++ lib/lovelace/commands/outside.ex
@@ -0,0 +1,20 @@
+defmodule Lovelace.Commands.Outside do
+  @moduledoc """
+  Define and handles external commands!
+  """
+
+  # Notice that here we just `use` Commander. Router is only
+  # used to map commands to actions. It's best to keep routing
+  # only in App.Commands file. Commander gives us helpful
+  # macros to deal with Nadia functions.
+  use Lovelace.Commander
+
+  # Functions must have as first parameter a variable named
+  # update. Otherwise, macros (like `send_message`) will not
+  # work as expected.
+  def outside(update) do
+    Logger.log(:info, "Command /outside")
+
+    send_message("This came from a separate module.")
+  end
+end
