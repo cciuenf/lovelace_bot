2da873d33c48e262730a0b966d357d8735b2a5d9
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-01-30 21:44:56 -0300

    bot entrypoint and main functions

--- lib/lovelace/application.ex
+++ lib/lovelace/application.ex
@@ -1,19 +1,28 @@
 defmodule Lovelace.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   @impl true
   def start(_type, _args) do
+    bot_name = Application.get_env(:app, :bot_name)
+
+    unless String.valid?(bot_name) do
+      IO.warn("""
+      Env not found Application.get_env(:app, :bot_name)
+      This will give issues when generating commands
+      """)
+    end
+
+    if bot_name == "" do
+      IO.warn("An empty bot_name env will make '/anycommand@' valid")
+    end
+
     children = [
-      # Starts a worker by calling: Lovelace.Worker.start_link(arg)
-      # {Lovelace.Worker, arg}
+      {Lovelace.Poller, []},
+      {Lovelace.Matcher, []}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Lovelace.Supervisor]
     Supervisor.start_link(children, opts)
   end
---
+++ lib/lovelace/matcher.ex
@@ -0,0 +1,32 @@
+defmodule Lovelace.Matcher do
+  @moduledoc """
+  Here is where the commands are dispatched
+  """
+
+  use GenServer
+
+  alias Lovelace.Commands
+
+  # Server
+
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok) do
+    {:ok, 0}
+  end
+
+  def handle_cast(message, state) do
+    message
+    |> Commands.match_message()
+
+    {:noreply, state}
+  end
+
+  # Client
+
+  def match(message) do
+    GenServer.cast(__MODULE__, message)
+  end
+end
