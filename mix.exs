d74d0812e0e944d7a9c5ac4c5f69d9c3a47153ef
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-02-16 11:11:43 -0300

    corrected project version and CI

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lovelace.MixProject do
   def project do
     [
       app: :lovelace,
-      version: "0.1.0",
+      version: "2.0.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
