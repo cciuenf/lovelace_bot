a45fd56918c829d0b070af6ad3de17e8ec8d4732
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-02-16 21:16:32 -0300

    error on prod config

--- config/config.exs
+++ config/config.exs
@@ -11,14 +11,6 @@ config :lovelace,
   ecto_repos: [Lovelace.Repo],
   generators: [binary_id: true]
 
-# Configures the endpoint
-config :lovelace, LovelaceWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "+41x5h8Xbn3ilu0YZ5UShcHI2/qhY3JGZpT8ockkzdkTHahMJe177aE2dcyQ5CAn",
-  render_errors: [view: LovelaceWeb.ErrorView, accepts: ~w(html json), layout: false],
-  pubsub_server: Lovelace.PubSub,
-  live_view: [signing_salt: "XP1985o+"]
-
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
@@ -30,6 +22,15 @@ config :phoenix, :json_library, Jason
 config :tesla, adapter: Tesla.Adapter.Hackney
 
 config :lovelace, pubsub_channel: Lovelace.PubSub
+
+# Configures the endpoint
+
+config :lovelace, LovelaceWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "+41x5h8Xbn3ilu0YZ5UShcHI2/qhY3JGZpT8ockkzdkTHahMJe177aE2dcyQ5CAn",
+  render_errors: [view: LovelaceWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Lovelace.PubSub,
+  live_view: [signing_salt: "XP1985o+"]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- lib/lovelace_web/controllers/telegram_integration_controller.ex
+++ lib/lovelace_web/controllers/telegram_integration_controller.ex
@@ -8,6 +8,8 @@ defmodule LovelaceWeb.TelegramIntegrationController do
   # we only match messages that start with a /, so we don't waste computer
   # power for messages that don't matter
   def webhook(conn, %{"message" => %{"text" => "/" <> _} = params}) do
+    IO.inspect(params)
+
     with {:ok, message} <- Telegram.build_message(params),
          :ok <- Telegram.enqueue_processing!(message) do
       Logger.info("Message enqueued for later processing")
