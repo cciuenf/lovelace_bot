f0c6e8ae72b375a623179f36ae35d2ae51cc62e6
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-02-18 14:28:02 -0300

    production version up and running

--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,9 @@
 import Config
 
-app_host = System.get_env("HOST", "0.0.0.0")
+app_host = System.get_env("HOST", "lovelace-szh7fuhjxa-ue.a.run.app")
 app_port = System.get_env("PORT", "8443") |> String.to_integer()
 
 config :lovelace, LovelaceWeb.Endpoint,
-  url: [host: app_host, port: app_port],
+  url: [host: app_host, port: app_port, scheme: "https"],
   check_origin: true,
   server: true
