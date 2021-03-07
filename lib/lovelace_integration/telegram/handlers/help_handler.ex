578a72333f11bf291bc1e7d6333ea0448ab53b57
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-03-06 23:07:39 -0300

    fixed help command

--- lib/lovelace_integration/telegram/handlers/help_handler.ex
+++ lib/lovelace_integration/telegram/handlers/help_handler.ex
@@ -10,9 +10,7 @@ defmodule LovelaceIntegration.Telegram.Handlers.HelpHandler do
     """
     Olá! Sou a lovelace_bot e estou aqui para te ajudar!
 
-    Dependendo do seu papel no grupo, posso realizar diferentes tarefas!
-
-    Case você seja um professor, todos os comandos podem ser executados!
+    Dependendo do seu papel no grupo, você poderá realizar diferentes tarefas!
 
     Veja algumas delas:
 
@@ -21,26 +19,28 @@ defmodule LovelaceIntegration.Telegram.Handlers.HelpHandler do
     WIP...
 
     <b>Se voê for um admin</b>
-    1. /promover <menções> -> promove um ou mais usuaŕios
-    2. /rebaixar <menções> -> rebaixa um ou mais usuaŕios
-    3. /banir <menções> -> bane um ou mais usuários
+    1. /promover |menções| -> promove um ou mais usuários
+    2. /rebaixar |menções| -> rebaixa um ou mais usuários
+    3. /banir |menções| -> bane um ou mais usuários
     4. /verificar -> mostra um pequeno relatório do grupo
 
     <b>Se você for aluno</b>
     1. /ajuda -> mostra essa mensagem
     2. /agenda -> mostra os próximos eventos do curso (WIP...)
     3. /desafios -> lista todos os desafios da Lovelace
-    4. /desafio <número> -> mostra apenas um desafio
+    4. /desafio |número| -> mostra apenas um desafio
     5. /ranking -> mostra o ranking geral dos desafios
-    6. /ranking <número> -> mostra os primeiros N desafiantes
+    6. /ranking |número| -> mostra os primeiros N desafiantes
+
+    Caso você seja um professor, todos os comandos podem ser executados!
     """
   end
 
-  def handle(%Message{chat_id: c_id, message_id: m_id}) do
+  def handle(%Message{} = msg) do
     %{
-      chat_id: c_id,
+      chat_id: msg.chat_id,
       parse_mode: "HTML",
-      reply_to_message_id: m_id,
+      reply_to_message_id: msg.message_id,
       text: help()
     }
     |> Client.send_message()
